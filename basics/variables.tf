40a0f127f834a86565fab0e93a307d305929355d
Chen <cheneus@gmail.com>
2020-02-24 07:48:17 -0600

    update env and use map for dev/prod

--- basics/variables.tf
+++ basics/variables.tf
@@ -1,3 +1,7 @@
+variable "env" {
+  description = "env: dev or prod"
+}
+
 #Define variables
 variable "image_name" {
   description = "Image for container."
@@ -5,13 +9,21 @@ variable "image_name" {
 }
 
 variable "container_name" {
+  type        = map
   description = "Name of the container."
-  # default     = "g-blog"
+  default = {
+    dev  = "blog_dev"
+    prod = "blog_prod"
+  }
 }
 
 variable "image_name" {
+  type        = map
   description = "Image for container."
-  default     = "ghost:latest"
+  default = {
+    dev  = "ghost:latest"
+    prod = "ghost:alpine"
+  }
 }
 
 variable "int_port" {
