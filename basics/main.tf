61b6a6060a65cfe856b9e7970569edba85688c3e
Chen <cheneus@gmail.com>
2020-02-24 07:54:08 -0600

    using lookup via env

--- basics/main.tf
+++ basics/main.tf
@@ -1,15 +1,16 @@
 # Download the latest Ghost image
 resource "docker_image" "image_id" {
-  name = var.image_name
+  # name = var.image_name
+  name = lookup(var.image_name, var.env)
 }
 
 # Start the Container
 resource "docker_container" "container_id" {
-  name  = var.container_name
+  name  = lookup(var.container_name, var.env) #var.container_name
   image = docker_image.image_id.latest
   ports {
     internal = var.int_port
-    external = var.ext_port
+    external = lookup(var.ext_port, var.env) #var.ext_port
   }
 }
 
