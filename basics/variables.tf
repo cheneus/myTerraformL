58697f82dfb6279eaeb2a6df1bc6f5d89eae38d7
Chen <cheneus@gmail.com>
2020-02-24 07:31:32 -0600

    remove default variables

--- basics/variables.tf
+++ basics/variables.tf
@@ -6,15 +6,15 @@ variable "image_name" {
 
 variable "container_name" {
   description = "Name of the container."
-  default     = "g-blog"
+  # default     = "g-blog"
 }
 
 variable "int_port" {
   description = "Internal port for container."
-  default     = "2368"
+  # default     = "2368"
 }
 
 variable "ext_port" {
   description = "External port for container."
-  default     = "80"
+  # default     = "80"
 }
